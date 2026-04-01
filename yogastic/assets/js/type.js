fd2129f5320d3e448155874ba4f95c350b110eb6
Miryldeleon <90466778+Miryldeleon@users.noreply.github.com>
2026-04-01 20:55:51 +0800

    feat: Initial commit of Next Life Ministries website

---
+++ yogastic/assets/js/type.js
@@ -0,0 +1,6 @@
+window.ityped.init(document.querySelector('.ityped'),{
+    strings: ['YOUR COSMIC ADVENTURE TO PARADISE BEGINS NOW.'],
+    typeSpeed: 200,
+    backSpeed: 150,
+    loop: false
+})
